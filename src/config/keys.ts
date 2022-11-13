e876177dee992a953c07be2470d20b925f4d529c
AnasBeast <boussehminea@gmail.com>
2022-11-14 00:39:06 +0100

    Keys

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,7 +1,7 @@
 export default {
   // For Mac
   mongoURI:
-    'mongodb://linroot:DU123iHqTUug7YkQ@lin-8322-15958.servers.linodedb.net/?authMechanism=DEFAULT&tls=true&tlsCAFile=%2FUsers%2Fmuhammadawais%2FDownloads%2FWaveMdatabases-ca-certificate.cer',
+    'mongodb://linroot:DU123iHqTUug7YkQ@lin-8322-15958.servers.linodedb.net:27017/?authMechanism=DEFAULT&tlsCAFile=WaveMdatabases-ca-certificate.cer&tls=true',
   // For Windows
   // mongoURI:
   //   'mongodb://linroot:DU123iHqTUug7YkQ@lin-8322-15958.servers.linodedb.net/?authMechanism=DEFAULT&tls=true&tlsCAFile=C%3A%5CUsers%5CHP%5CDownloads%5CWaveMdatabases-ca-certificate.cer',
